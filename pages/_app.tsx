eb2100a1525823af5b2183128caefa9cf8d13394
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-07-30 01:29:02 +0800

    added nextauth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import { reducer, StateProvider } from '@/state';
+import { SessionProvider } from "next-auth/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <StateProvider reducer={reducer}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </StateProvider>
+    <SessionProvider session={pageProps.session}>
+      <StateProvider reducer={reducer}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </StateProvider>
+    </SessionProvider>
   );
 }
 
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma) as Adapter,
+    theme: {
+        colorScheme: "light",
+    },
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+});
